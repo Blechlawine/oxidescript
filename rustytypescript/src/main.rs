420f397f1db8ffc423b38bf59facb9de7934f4c8
Blechlawine <marczinser@gmx.de>
2023-05-18 16:41:24 +0200

    feat: workspaces, tree-sitter for parsing

--- rustytypescript/src/main.rs
+++ rustytypescript/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser as ClapParser;
 use std::{fs::read_to_string, path::PathBuf};
-
-mod parser;
+use tree_sitter::Parser;
+use tree_sitter_rustytypescript::language;
 
 #[derive(ClapParser, Debug)]
 #[command(version)]
@@ -12,12 +12,16 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    dbg!(&args);
+
+    let mut parser = Parser::new();
+    let language = language();
+    parser.set_language(language).unwrap();
 
     for file in args.inputs {
         let loaded_file = load_file(PathBuf::from(file));
         dbg!(&loaded_file);
-        parser::parse_string(loaded_file).unwrap();
+        let tree = parser.parse(loaded_file, None);
+        dbg!(tree);
     }
 }
 
---
+++ tree-sitter-rustytypescript/grammar.js
@@ -0,0 +1,7 @@
+module.exports = grammar({
+    name: "rustytypescript",
+
+    rules: {
+        source_file: ($) => "hello",
+    },
+});
