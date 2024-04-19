8827ed55cd6b69b79d24b45780064a653ec16a0a
Blechlawine <marczinser@gmx.de>
2024-04-19 21:37:20 +0200

    feat: move cli into separate package

--- oxidescript-cli/src/main.rs
+++ oxidescript-cli/src/main.rs
@@ -1,20 +1,20 @@
-use clap::Parser as ClapParser;
-use lexer::tokens::Tokens;
 use std::{fs::read_to_string, path::PathBuf};
 
-use crate::compiler::JavascriptCompiler;
-use crate::lexer::Lexer;
-use crate::parser::Parser;
+use clap::Parser as ClapParser;
+use oxidescript::{
+    compiler::JavascriptCompiler,
+    lexer::{tokens::Tokens, Lexer},
+    parser::Parser,
+};
 
-pub mod compiler;
-pub mod lexer;
-pub mod parser;
-
-#[derive(ClapParser, Debug)]
+#[derive(clap::Parser, Debug)]
 #[command(version)]
 struct Args {
     #[arg(last = true)]
     inputs: Vec<String>,
+
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    verbose: bool,
 }
 
 fn main() {
@@ -22,15 +22,21 @@ fn main() {
 
     for file in args.inputs {
         let loaded_file = load_file(PathBuf::from(file));
-        dbg!(&loaded_file);
+        if args.verbose {
+            println!("Loaded file: {:?}", &loaded_file);
+        }
 
         let (unlexed, tokens) = Lexer::lex_tokens(loaded_file.as_bytes()).unwrap();
-        dbg!(unlexed);
-        dbg!(&tokens);
+        if args.verbose {
+            println!("Unlexed: {:?}", unlexed);
+            println!("Tokens: {:#?}", tokens);
+        }
 
         let (unparsed, ast) = Parser::parse(Tokens::new(&tokens)).unwrap();
-        dbg!(unparsed);
-        dbg!(&ast);
+        if args.verbose {
+            println!("Unparsed: {:?}", unparsed);
+            println!("AST: {:#?}", &ast);
+        }
 
         let compiled = JavascriptCompiler::compile(ast);
         println!("{}", compiled);
